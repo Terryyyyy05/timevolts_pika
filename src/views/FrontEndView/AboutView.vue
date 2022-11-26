cb1326ca90bc9a3e0e4dedc1350a08d3e3388ec4
irene0103 <miki1234594@gmail.com>
2022-11-27 02:23:23 +0800

    add font and adjust layout location

--- src/views/FrontEndView/AboutView.vue
+++ src/views/FrontEndView/AboutView.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="about">
     <h1>關於我們</h1>
-    <p>dsjifajdsifjidsojfoia</p>
+    <p>About us</p>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+</script>
+
+<style scope lang="scss">
+@import "../../assets/css/app.scss";
+
+h1{
+	font-family: 'Cube11';
+  color: antiquewhite;
+}
+p{
+	font-family: '8bit';
+}
+</style>
