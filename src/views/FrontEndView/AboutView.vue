0c33df5dcef5e9624d0f57c56f95297feb1b6d39
irene0103 <miki1234594@gmail.com>
2022-11-28 01:04:13 +0800

    update button

--- src/views/FrontEndView/AboutView.vue
+++ src/views/FrontEndView/AboutView.vue
@@ -2,6 +2,11 @@
   <div class="about">
     <h1>關於我們</h1>
     <p>About us</p>
+    <button class="primary">click me</button>
+    <button class="secondary">click me</button>
+    <button class="lightbox">click me</button>
+    <button class="disable">click me</button>
+
   </div>
 </template>
 
@@ -20,4 +25,21 @@ h1{
 p{
 	font-family: '8bit';
 }
+.primary{
+  @include btn-primary;
+  margin: 20px;
+}
+.secondary{
+  @include btn-secondary;
+  margin: 20px;
+}
+.lightbox{
+  @include btn-lightbox;
+  margin: 20px;
+}
+.disable{
+  @include btn-disable;
+  margin: 20px;
+}
+
 </style>
