ce06e6922e68ea091f01babfc3e698b353d7966f
Terryyyyy05 <k0928647020@gmail.com>
2022-11-29 00:59:41 +0800

    Terry add css_reset & modify some font rules

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
 }
 
 nav {
