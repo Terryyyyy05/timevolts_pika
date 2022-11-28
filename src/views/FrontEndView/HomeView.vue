75f11ff007eddf4040c5e7f871f5e44317e13fac
Terryyyyy05 <k0928647020@gmail.com>
2022-11-29 01:18:31 +0800

    QQ

--- src/views/FrontEndView/HomeView.vue
+++ src/views/FrontEndView/HomeView.vue
@@ -3,29 +3,29 @@
     <!-- <img alt="Vue logo" src="../../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
-  <div class="bg_dark">
-    <h1 >
-    TimeVolts
-    <span>時萬伏特</span>
-    </h1>
-  </div>
-  <div class="bg_dark p_md ">
-    <p>Last login: Wed Nov 16 10:53:48</p>
-    <p>Restored session:</p>
-    <p>TimeVolts ~ %</p>
-    <p>
-      .<br>
-      .<br>
-      .<br>
-      假文字假文字假文字假文字假文字假文字假文字<br>
-      假文字假文字假文字假文字假文字假文字假文字假文字<br>
-      假文字假文字假文字假文字假文字假文字假文字假文字假文字假文字
-    </p>
-  </div>
-  <div>
-    <strong>現在，我們實現了時光旅行</strong>
-    <img src="" alt="">
-  </div>
+    <div class="bg_dark">
+      <h1>
+        TimeVolts
+        <span>時萬伏特</span>
+      </h1>
+    </div>
+    <div class="bg_dark p_md">
+      <p>Last login: Wed Nov 16 10:53:48</p>
+      <p>Restored session:</p>
+      <p>TimeVolts ~ %</p>
+      <p>
+        .<br />
+        .<br />
+        .<br />
+        假文字假文字假文字假文字假文字假文字假文字<br />
+        假文字假文字假文字假文字假文字假文字假文字假文字<br />
+        假文字假文字假文字假文字假文字假文字假文字假文字假文字假文字
+      </p>
+    </div>
+    <div>
+      <strong>現在，我們實現了時光旅行</strong>
+      <img src="" alt="" />
+    </div>
   </div>
 </template>
 
@@ -33,11 +33,10 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
-
