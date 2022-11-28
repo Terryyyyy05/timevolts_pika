81d015ac1388a17163611e53dd94f1c3be721a8f
lone714 <lone714@gmail.com>
2022-11-28 21:39:35 +0800

    fix HomeView.vue

--- src/views/FrontEndView/HomeView.vue
+++ src/views/FrontEndView/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <div>
+    <h1>
+    TimeVolts
+    <span>時萬伏特</span>
+    </h1>
+  </div>
+  <div>
+    <p>Last login: Wed Nov 16 10:53:48</p>
+    <p>Restored session:</p>
+    <p>TimeVolts ~ %</p>
+    <p>
+      .<br>
+      .<br>
+      .<br>
+      假文字假文字假文字假文字假文字假文字假文字<br>
+      假文字假文字假文字假文字假文字假文字假文字假文字<br>
+      假文字假文字假文字假文字假文字假文字假文字假文字假文字假文字
+    </p>
+  </div>
+  <div>
+    <strong>現在，我們實現了時光旅行</strong>
+    <img src="" alt="">
+  </div>
   </div>
 </template>
 
