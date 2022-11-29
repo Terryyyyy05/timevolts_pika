f05e09f7fc1267c8fba24986d912786735b2da80
Chris Huang <114379646+overcast3579@users.noreply.github.com>
2022-11-29 20:04:20 +0800

    add component

---
+++ src/components/itiniraryCrslBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="itinCrslBtn">
+    <p class="p_md">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'itiniraryCrslBtn',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/css/app.scss";
+    p{
+        text-align: center;
+        color:map-get($color, primary)
+    }
+
+</style>
