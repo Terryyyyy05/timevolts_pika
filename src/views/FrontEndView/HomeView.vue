b89aa2be2799ff285431b01766b7029d51145edd
lone714 <lone714@gmail.com>
2022-11-29 19:00:55 +0800

    fix homeView，幫上一個做紀錄，字體副檔名要大寫

--- src/views/FrontEndView/HomeView.vue
+++ src/views/FrontEndView/HomeView.vue
@@ -2,29 +2,35 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
-    <div class="homeTitle bg_dark">
-      <h1>
-        TimeVolts
-        <span>時萬伏特</span>
-      </h1>
-    </div>
-    <div class="bg_dark p_md">
-      <p>Last login: Wed Nov 16 10:53:48</p>
-      <p>Restored session:</p>
-      <p>TimeVolts ~ %</p>
-      <p>
-        .<br />
-        .<br />
-        .<br />
-        假文字假文字假文字假文字假文字假文字假文字<br />
-        假文字假文字假文字假文字假文字假文字假文字假文字<br />
-        假文字假文字假文字假文字假文字假文字假文字假文字假文字假文字
-      </p>
-    </div>
-    <div>
-      <strong>現在，我們實現了時光旅行</strong>
-      <img src="" alt="" />
+    <div class="container">
+      <!-- <div class="titleText"> -->
+        <div class="sideTotem">
+          <img src="@/assets/image/home/sideTotem.svg" alt="時萬伏特-圖樣">
+        </div>
+        <div class="homeTitle bg_dark">
+          <h1>
+            TimeVolts
+            <span>時萬伏特</span>
+          </h1>
+        </div>
+        <div class="bg_dark p_md">
+          <p>Last login: Wed Nov 16 10:53:48</p>
+          <p>Restored session:</p>
+          <p>TimeVolts ~ %</p>
+          <p>
+            .<br />
+            .<br />
+            .<br />
+            假文字假文字假文字假文字假文字假文字假文字<br />
+            假文字假文字假文字假文字假文字假文字假文字假文字<br />
+            假文字假文字假文字假文字假文字假文字假文字假文字假文字假文字
+          </p>
+        </div>
+        <div>
+          <strong>現在，我們實現了時光旅行</strong>
+          <img src="" alt="" />
+        </div>
+      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -41,22 +47,37 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped >
+<style scoped lang="scss">
 @import "@/assets/css/app.scss";
-.homeTitle{
-  margin: 20px;
-  text-align: center;
-  h1{
-    span{
-      display: none;
-    }
-  }
-}
 .home{
-  display: flex;
-  flex-direction: column;
-  border: 1px map-get($color , primary );
-  color: map-get($color, primary) ;
-  
+  // display: flex;
+  .container{
+    color: map-get($color, primary) ;
+      display: grid;
+      grid-template-columns: 2fr 4.5fr 4.5fr 1fr;
+      grid-template-rows: 2fr 2fr 1fr;
+    // border: 1px map-get($color , primary );
+    .sideTotem{
+      // width: 8.333333;
+      grid-column:1 ;
+      grid-row: 1/3;
+      min-height: 50vh;
+      img{
+        width: 100%;
+        max-height:80vh ;
+      }
+    }
+    .titleText{
+      .homeTitle{
+        text-align: center;
+        h1{
+          span{
+            display: none;
+          }
+        }
+      }
+    }
+
+  }
 }
 </style>
