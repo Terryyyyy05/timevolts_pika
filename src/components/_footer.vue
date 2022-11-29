bb9ebd556d7b4ced37777812bc589aeaf1f3f6cf
Melodyhsu88 <114372477+Melodyhsu88@users.noreply.github.com>
2022-11-30 00:43:19 +0800

    add header and footer

---
+++ src/components/_footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        
+ 
+ 
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+    
+</style>
