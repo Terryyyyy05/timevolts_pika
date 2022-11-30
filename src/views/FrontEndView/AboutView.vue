910c01e83c6d8f56077d4087312b23beb61cce80
Terryyyyy05 <k0928647020@gmail.com>
2022-11-30 22:11:46 +0800

    add file

--- src/views/FrontEndView/AboutView.vue
+++ src/views/FrontEndView/AboutView.vue
@@ -3,10 +3,10 @@
     <h1>關於我們</h1>
     <p>About us</p>
     <p class="font">完盪了 按鈕字體跑不出來hihihi</p>
-    <button class="primary">主要按鈕</button>
-    <button class="secondary">次要按鈕</button>
-    <button class="lightbox">燈箱按鈕</button>
-    <button class="disable">停用狀態</button>
+    <button class="btn-primary">主要按鈕</button>
+    <button class="btn-secondary">次要按鈕</button>
+    <button class="btn-lightbox">燈箱按鈕</button>
+    <button class="btn-disable">停用狀態</button>
 
   </div>
 </template>
@@ -23,24 +23,24 @@
 <style scoped lang="scss">
 @import "@/assets/css/app.scss";
 
-.primary{
-  @include btn-primary;
-  margin: 20px;
-}
+// .primary{
+//   @include btn-primary;
+//   margin: 20px;
+// }
 
-.secondary{
-  @include btn-secondary;
-  margin: 20px;
-}
-.lightbox{
-  @include btn-lightbox;
-  margin: 20px;
-}
+// .secondary{
+//   @include btn-secondary;
+//   margin: 20px;
+// }
+// .lightbox{
+//   @include btn-lightbox;
+//   margin: 20px;
+// }
 
-.disable{
-  @include btn-disable;
-  margin: 20px;
-}
+// .disable{
+//   @include btn-disable;
+//   margin: 20px;
+// }
 
 
 </style>
