789e848218816bd4a57309fa1351179ff87cf319
boazzzzz <boaz0105@gmail.com>
2022-12-01 21:03:48 +0800

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// SCSS
 import "@/assets/css/app.scss";
 
+// Import Components
 import TheHeading from "./components/layout/TheHeading.vue";
 
+// Vue 實體
 const app = createApp(App);
 
 app.use(store);
