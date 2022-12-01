cc301a3b07cfb327c05a3af1a0ac10afe702b0b9
boazzzzz <boaz0105@gmail.com>
2022-12-01 16:57:04 +0800

    add components - the heading

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import store from "./store";
 
 import "@/assets/css/app.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+import TheHeading from "./components/layout/TheHeading.vue";
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.component("the-heading", TheHeading);
+
+app.mount("#app");
