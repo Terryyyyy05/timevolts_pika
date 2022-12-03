7a1f0609cc0af48fe720b05793b80ac405761b97
boazzzzz <boaz0105@gmail.com>
2022-12-03 22:45:02 +0800

    add some stuff

---
+++ src/components/history/UI/HistoryBanner.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="banner">
+      <slot></slot>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.banner {
+   position: relative;
+   &::before {
+      content: "";
+      background-position: center;
+      background-size: cover;
+      background-attachment: fixed;
+      background-image: url("~@/assets/image/history/test.png");
+      // position: absolute;
+      display: block;
+      left: 0;
+      top: 0;
+      width: 100vw;
+      height: 100vh;
+      z-index: -1;
+   }
+}
+</style>
