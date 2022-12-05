d605a5ffb5f3b0aadf8686a87da618108357bb2a
VitoProgramming <vito.formosa@outlook.com>
2022-12-06 02:19:36 +0800

    conflict

--- src/views/FrontEndView/MemberProfileView.vue
+++ src/views/FrontEndView/MemberProfileView.vue
@@ -8,51 +8,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import MemberSidebar from "@/components/member/MemberSidebar.vue";
-import MemberBox from "@/components/member/MemberBox.vue";
-import MemberModule from "@/components/member/MemberModule.vue";
-export default {
-  components: {
-    MemberSidebar,
-    MemberBox,
-  },
-  data() {
-    return {
-      memberDataList: [
-        {
-          id: 1,
-          memberData: "會員資料",
-        },
-        {
-          id: 2,
-          memberData: "行程訂單",
-        },
-        {
-          id: 3,
-          memberData: "商品訂單",
-        },
-        {
-          id: 4,
-          memberData: "我的折扣券",
-        },
-        {
-          id: 5,
-          memberData: "行程蒐藏",
-        },
-        {
-          id: 6,
-          memberData: "商品蒐藏",
-        },
-      ],
-    };
-  },
-  methods: {
-    changeSidebar(e) {
-      console.log(e);
-    },
-    MemberModule
-=======
 import innerpageHeader from "@/components/innerpageHeader.vue";
 import MemberSidebar from "@/components/member/MemberSidebar.vue";
 import MemberBoxProfile from "@/components/member/memberBox/MemberBoxProfile.vue";
@@ -61,7 +16,6 @@ export default {
     innerpageHeader,
     MemberSidebar,
     MemberBoxProfile,
->>>>>>> dev_vito
   },
 };
 </script>
