3e402b441861ce858de695ffee4ae3d9387609bf
Chris Huang <114379646+overcast3579@users.noreply.github.com>
2022-12-07 10:28:00 +0800

    add backend manager login

--- src/views/BackEndView/manageloginView.vue
+++ src/views/BackEndView/manageloginView.vue
@@ -1,11 +1,44 @@
 <template>
-    <div>
-        我們下次見~
+    <div class="wrap">
+        <div class="title">管理員登入</div>
+        <div class="accountwrap">
+            <label>帳號:<input type="text"></label>   
+        </div>
+        <div class="pswwrap">
+            <label>密碼:<input type="password"></label>  
+        </div>
+        <div class="btnwrap">
+            <button value="登入">登入</button>
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
-    div{
-        margin: 10px;
+@import "@/assets/css/utils/variables";
+
+    .wrap{
+        width: 300px;
+        height: fit-content;
+        margin: calc(50vh - 150px) auto;
+        border: 2px solid map-get($color, "primary");
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-self: center;
+        font-size: 24px;
         color: #ffe1b5;
+    }
+    .title, .accountwrap, .pswwrap, .btnwrap{
+        width: 100%;
+        height: 30px;
+        padding: 10px 0;
+        margin: 0 10px;
+    }
+    .btnwrap{
+        display: flex;
+        justify-content: end;
+    }
+    button{
+        width: 20%;
+        margin-right: 20px;
     }
 </style>
