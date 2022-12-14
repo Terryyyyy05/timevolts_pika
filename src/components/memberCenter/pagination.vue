e854e9ad1bd71a149ebe77debcb3883be05f9a1c
Melodyhsu88 <114372477+Melodyhsu88@users.noreply.github.com>
2022-12-14 20:55:07 +0800

    all

---
+++ src/components/memberCenter/pagination.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="page">
+        <ul class="pagination" v-for="page in 5" :key="page">
+          <li>{{page}}</li>
+        </ul>
+      </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/css/utils/variables';
+
+
+.page{
+    text-align: center;
+  }
+  .pagination{
+    width: 15px;
+    display: inline-block;
+    border: 1px solid map-get($color, accent);
+    color: map-get($color, accent);
+  }
+</style>
