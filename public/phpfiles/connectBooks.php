7903924f399b5ed085cb33bdb5eb5d913a9df74f
irene0103 <miki1234594@gmail.com>
2022-12-20 16:58:19 +0800

    ddddd

--- public/phpfiles/connectBooks.php
+++ public/phpfiles/connectBooks.php
@@ -1,7 +1,8 @@
 <?php
+
   $dbname = "tibamefe_cgd103g3";
 	$user = "root";
-	$password = "cgd10307";
+	$password = "CGD1032017";
 	$port = '3306';
 
 	$dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
