d733ca6dfbe792346839c3748be8b15a79b02256
lone714 <lone714@gmail.com>
2022-12-21 13:10:37 +0800

    fix HomeNews

--- src/components/home/HomeNews.vue
+++ src/components/home/HomeNews.vue
@@ -26,38 +26,7 @@ export default {
     return {
       NewsData: [],
 
-      newsData: [
-        {
-          id: 1,
-          title: "新商品出爐囉!!",
-          date: "2022-12-11",
-          hashtag: "#新品上架",
-          content:
-            "超過上百種語言翻譯，打破溝通障礙，運用本公司獨家秘方【翻譯蒟蒻】，精準翻出生活化的當地語言，就像學會當地語言一樣與人流暢對話。",
-          content_box: "",
-          img: require(`@/assets/image/news/jelly.jpeg`),
-        },
-        {
-          id: 2,
-          title: "穿梭於史前時代",
-          date: "2022-12-10",
-          hashtag: "#經典行程 #高度危險",
-          content:
-            "人類的歷史分成：史前時代及信使時代。「文字的出現」為畫分的依據，史前時代沒有文字，信使時代為文字開始。今天，我們將回到史前時代，建立屬於自己的莊園，面對嚴苛的大自然及猛獸努力存活下去。",
-          content_box: "",
-          img: require(`@/assets/image/news/prehistoric.jpg`),
-        },
-        {
-          id: 3,
-          title: "官網維護公告",
-          date: "2022-11-09",
-          hashtag: "#公告 #維修中",
-          content:
-            "親愛的用戶您好，為提高您的網站使用品質，本公司將預計2022-09-15進行系統維護作業，期間會造成部分網頁無法使用，造成您的不便，敬請見諒!",
-          content_box: "",
-          img: require(`@/assets/image/news/system.png`),
-        },
-      ],
+      newsData: [],
       imgsrc: require("@/assets/image/home/icon/icon_1.svg"),
     };
   },
@@ -66,7 +35,7 @@ export default {
   },
   methods: {
     getHomeNewData() {
-      fetch("http://localhost/timevolts_pika/public/phpfiles/getHomeNew.php")
+      fetch("http://localhost/timevolts_pika/public/phpfile/getHomeNew.php")
         .then((res) => res.json())
         .then((jsonData) => {
           this.NewsData = jsonData;
