ce47b375be4bf7b55bf2a6861995e0e61d344196
lone714 <lone714@gmail.com>
2022-12-21 16:24:03 +0800

    fix

--- public/phpfile/getHomeNew.php
+++ public/phpfile/getHomeNew.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin:*');
 header("Content-Type:application/json;charset=utf-8");
 require_once("./php_connect_books/connectBooks.php");
+// 抓時間最近的前三筆
 $sql = "select * from news where news_status = 1 order by news_add_date desc limit 3";
 $products = $pdo->query($sql);
 $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
