2674ddd06b1abe36e45d20dd85c2490507f54608
irene0103 <miki1234594@gmail.com>
2022-12-28 23:04:13 +0800

    上線嚕~

--- phpfile/getNews.php
+++ phpfile/getNews.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin:*');
 header("Content-Type:application/json;charset=utf-8");
-require_once("./php_connect_books/connectBooks.php");
+require_once("./php_connect_books/connect_cgd103g3.php");
 $sql = "select * from news order by news_add_date DESC" ; 
 $products = $pdo->query($sql);
 $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
--- src/assets/js/commom.js
+++ src/assets/js/commom.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "/api_server/";
+// export const BASE_URL = "/api_server/";
 
-// export const BASE_URL = 'https://tibamef2e.com/cgd103/g3/phpfile/';
+export const BASE_URL = 'https://tibamef2e.com/cgd103/g3/phpfile/';
