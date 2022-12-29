eca02c21e79df0d9650d85a621ebc6e5331027bf
Terryyyyy05 <k0928647020@gmail.com>
2022-12-29 19:26:54 +0800

    慘

--- src/assets/js/img_path.js
+++ src/assets/js/img_path.js
@@ -1,6 +1,9 @@
 export const IMG_URL = (pro_img_name) => {
-  return require(`@/assets/image/new_img/${pro_img_name}`);
+  // return require(`@/assets/image/new_img/${pro_img_name}`);
 
   // 上線版
-  // return require(`./img/${pro_img_name}`);
+  // return require(`/public/new_img2/${pro_img_name}`);
+  // return `/public/image`;
+
+  return `./new_img2/${pro_img_name}`;
 };
